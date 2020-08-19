b47211d5de1858f29254594b7e4bc1c89adc8352
Carson Davies <carsondavies@Carsons-MBP.T-mobile.com>
2020-08-19 09:02:04 -0600

    initial commit react 1 lecture

---
+++ src/components/Directory.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+
+
+class Directory extends Component {
+    constructor(){
+        super()
+        this.state = {
+            friends: [],
+            name: '',
+            image: ''
+        }
+    }
+
+    handleNameChange(e){
+        console.log(e.target.value)
+
+        this.setState({
+            name: e.target.value,
+        })
+    }
+
+    handleImageChange(e){
+        this.setState({
+            image: e.target.value
+        })
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        const newFriend = {
+            name: this.state.name,
+            image: this.state.image
+        }
+        this.setState({
+            friends: [...this.state.friends, newFriend]
+        })
+    }
+
+    render(){
+        const friendList = this.state.friends.map(element => {
+            return (
+            <div>
+                <img src={element.image}/>
+                <span>{element.name}</span>
+            </div>
+            )
+        })
+        
+        return ( 
+            <div>
+                <h1>a list of my friends</h1>
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input onChange={e => this.handleNameChange(e)} placeholder='Name'/>
+                    <input onChange={e => this.handleImageChange(e)} placeholder='Image'/>
+                    <button>Submit</button>
+                </form>
+                {friendList}
+            </div>
+    
+        )
+    }
+}
+
+export default Directory
